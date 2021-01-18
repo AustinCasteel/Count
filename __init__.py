86c8361e44c1fccc0cc349b40906705f9a447a2d
Austin Casteel <austin.casteel@gmail.com>
2021-01-18 10:04:37 -0600

    python

--- __init__.py
+++ __init__.py
@@ -20,13 +20,13 @@ class Count(plugin.SpeechHandlerPlugin):
     def handle(self, intent, mic):
         text = intent['input']
 
-        if text.startswith( 'COUNT TO' )
+        if text.startswith( 'COUNT TO' ):
             preprocess = text.split()
             number = preprocess [-1]
             for i in range(1, number+1, +1):
                 mic.say(str(i))
 
-        if text.startswith( 'COUNT FROM' )
+        if text.startswith( 'COUNT FROM' ):
             preprocess = text.split()
             number = preprocess [-1]
             for i in range(number, 0, -1):
